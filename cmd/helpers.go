a62e7aa567c0f8b0982cae8b718df9601801f48c
npikall <nikolas.pikall@gmail.com>
2026-01-09 17:14:15 +0100

    refactor: moved internal/echo into cmd/helpers

---
+++ cmd/helpers.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+// Common Colors
+var (
+	Cyan   = lipgloss.Color("14")
+	Yellow = lipgloss.Color("11")
+	Red    = lipgloss.Color("9")
+)
+
+// Common Styles
+var (
+	InfoStyle = lipgloss.NewStyle().Foreground(Cyan).Bold(true)
+	WarnStyle = lipgloss.NewStyle().Foreground(Yellow).Bold(true)
+	ErrStyle  = lipgloss.NewStyle().Foreground(Red).Bold(true)
+)
+
+// Print a message to stdout with colored prefix 'info:'
+func LogInfof(format string, v ...any) {
+	msg := fmt.Sprintf(format, v...)
+	fmt.Printf("%s: %s\n", InfoStyle.Render("info"), msg)
+}
+
+// Print a warning message to stdout with colored prefix 'warning:'
+func LogWarnf(format string, v ...any) {
+	msg := fmt.Sprintf(format, v...)
+	fmt.Printf("%s: %s\n", WarnStyle.Render("warning"), msg)
+}
+
+// A given Function must return no error.
+// When an error occurs the program is exited.
+func Must[T any](t T, err error) T {
+	if err != nil {
+		log.Fatalf("%s: %s", ErrStyle.Render("error"), err)
+	}
+	return t
+}
