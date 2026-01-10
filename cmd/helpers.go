cce3b191699869ab0d5ac85ef7d695ab04fb970e
Nikolas Pikall <nikolas.pikall@gmail.com>
2026-01-10 17:15:26 +0100

    refactor: change coloring

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -9,42 +9,54 @@ import (
 
 // Common Colors
 var (
-	Cyan   = lipgloss.Color("81")
-	Yellow = lipgloss.Color("3")
-	Red    = lipgloss.Color("124")
-	White  = lipgloss.Color("231")
+	Blue    = lipgloss.Color("81")
+	Yellow  = lipgloss.Color("3")
+	Red     = lipgloss.Color("124")
+	White   = lipgloss.Color("231")
+	Gray    = lipgloss.Color("245")
+	Magenta = lipgloss.Color("72")
+	Cyan    = lipgloss.Color("117")
 )
 
-// Common Styles
+// Styles for Messages to stdout/stderr
 var (
-	InfoStyle = lipgloss.NewStyle().Foreground(Cyan).Bold(true)
-	WarnStyle = lipgloss.NewStyle().Foreground(Yellow).Bold(true)
-	ErrStyle  = lipgloss.NewStyle().Foreground(Red).Bold(true)
-	HighStyle = lipgloss.NewStyle().Foreground(White).Bold(true)
+	InfoStyle    = lipgloss.NewStyle().Foreground(Blue).Bold(true)
+	WarnStyle    = lipgloss.NewStyle().Foreground(Yellow).Bold(true)
+	ErrStyle     = lipgloss.NewStyle().Foreground(Red).Bold(true)
+	HighStyle    = lipgloss.NewStyle().Foreground(White).Bold(true)
+	DefaultStyle = lipgloss.NewStyle().Foreground(Gray)
+)
+
+// Styles for the List Command
+var (
+	namespaceStyle = lipgloss.NewStyle().Bold(true).Foreground(Magenta).MarginTop(1)
+	packageStyle   = lipgloss.NewStyle().Bold(true).Foreground(Cyan)
+	versionStyle   = lipgloss.NewStyle().Foreground(Gray)
+	countStyle     = lipgloss.NewStyle().Foreground(Gray)
 )
 
 // Print a message to stdout with colored prefix 'info:'
 func LogInfof(format string, v ...any) {
 	msg := fmt.Sprintf(format, v...)
-	fmt.Printf("%s: %s\n", InfoStyle.Render("info"), msg)
+	fmt.Printf("%s: %s\n", InfoStyle.Render("info"), DefaultStyle.Render(msg))
 }
 
 // Print a warning message to stdout with colored prefix 'warning:'
 func LogWarnf(format string, v ...any) {
 	msg := fmt.Sprintf(format, v...)
-	fmt.Printf("%s: %s\n", WarnStyle.Render("warning"), msg)
+	fmt.Printf("%s: %s\n", WarnStyle.Render("warning"), DefaultStyle.Render(msg))
 }
 
 // Print an error message to stdout with colored prefix 'error:' without exiting
 func LogErrf(format string, v ...any) {
 	msg := fmt.Sprintf(format, v...)
-	fmt.Printf("%s: %s\n", ErrStyle.Render("error"), msg)
+	fmt.Printf("%s: %s\n", ErrStyle.Render("error"), DefaultStyle.Render(msg))
 }
 
 // Print an error message to stdout with colored prefix 'error:' without exiting
 func LogFatalf(format string, v ...any) {
 	msg := fmt.Sprintf(format, v...)
-	fmt.Printf("%s: %s\n", ErrStyle.Render("error"), msg)
+	fmt.Printf("%s: %s\n", ErrStyle.Render("error"), DefaultStyle.Render(msg))
 	os.Exit(1)
 }
 
