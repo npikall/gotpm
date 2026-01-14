9257fa7a27610b84fb64ad94d18412019c3ea3bf
npikall <nikolas.pikall@gmail.com>
2026-01-14 11:33:05 +0100

    refactor: use styling from charmbracelet/fang

--- cmd/root.go
+++ cmd/root.go
@@ -7,9 +7,10 @@ See the LICENSE file in the repository root for full license text.
 package cmd
 
 import (
+	"context"
 	"os"
 
-	cc "github.com/ivanpirog/coloredcobra"
+	"github.com/charmbracelet/fang"
 	"github.com/spf13/cobra"
 )
 
@@ -24,16 +25,7 @@ that installs Typst Packages onto your machine.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	cc.Init(&cc.Config{
-		RootCmd:  rootCmd,
-		Headings: cc.Green + cc.Bold,
-		Commands: cc.Cyan + cc.Bold,
-		Example:  cc.Italic,
-		ExecName: cc.Bold,
-		Flags:    cc.Cyan + cc.Bold,
-	})
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := fang.Execute(context.Background(), rootCmd); err != nil {
 		os.Exit(1)
 	}
 }
