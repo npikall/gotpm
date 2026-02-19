446a4e2415a83ff789ecd2664f7a9b0c7f41c6a9
npikall <nikolas.pikall@gmail.com>
2026-02-19 12:20:51 +0100

    fix(cmd): embed version

--- cmd/root.go
+++ cmd/root.go
@@ -39,7 +39,7 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	if err := fang.Execute(context.Background(), rootCmd); err != nil {
+	if err := fang.Execute(context.Background(), rootCmd, fang.WithVersion(getGoTPMVersion())); err != nil {
 		os.Exit(1)
 	}
 }
