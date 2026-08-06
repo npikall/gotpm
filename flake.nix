f58a8e99e385a3a368c68fc8347382e994b02870
Niko Pikall <166406805+npikall@users.noreply.github.com>
2026-08-06 15:23:43 +0200

    chore: add nix flake (#25)

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "GoTPM development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Go toolchain
+            go_1_26
+
+            # Development tools
+            gopls # Go language server
+            golangci-lint # Linter
+            gofumpt # Formatter (stricter than gofmt)
+            go-task # Taskrunner
+
+            # Additional tools
+            git # Version control
+            gh # GitHub CLI
+            svu # Semantic version utility
+          ];
+
+          shellHook = ''
+            if [ -n "$ZSH_VERSION" ]; then
+              export PROMPT="%F{green}(nix)%f %~ %# "
+            else
+              export PS1="\[\033[1;32m\](nix)\[\033[0m\] \w \$ "
+            fi
+
+            # Set Go environment variables
+            export CGO_ENABLED=0
+          '';
+        };
+      }
+    );
+}
