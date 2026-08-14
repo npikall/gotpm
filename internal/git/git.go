59e7008ea42e1f0e2a7b3c30984209aaea8a8131
npikall <nikolas.pikall@gmail.com>
2026-08-14 16:14:31 +0200

    refactor: add sparse cloning function

---
+++ internal/git/git.go
@@ -0,0 +1,108 @@
+package git
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/go-git/go-git/v6"
+	"github.com/go-git/go-git/v6/plumbing"
+	"github.com/go-git/go-git/v6/plumbing/client"
+	"github.com/go-git/go-git/v6/plumbing/object"
+	"github.com/go-git/go-git/v6/plumbing/protocol/packp"
+	"github.com/go-git/go-git/v6/plumbing/transport"
+	"github.com/go-git/go-git/v6/storage"
+)
+
+// SparseRepo represents a (large) Repo, that should be checked out sparsely
+type SparseRepo struct {
+	// The url of where the repo's remote
+	URL string
+	// Represents the path that should be checked out
+	Path string
+	// The Branch that should be checked out
+	Branch string
+}
+
+// SparseClone clones a repository from url into dst
+// and does a sparse-checkout on path
+func SparseClone(dst string, sr SparseRepo) error {
+	ctx := context.Background()
+	branch := plumbing.NewBranchReferenceName(sr.Branch)
+	opts := sparseCloneOptions(sr.URL, branch)
+
+	// Blobless Clone, because we fetch objects later
+	repo, err := git.PlainClone(dst, opts)
+	if err != nil {
+		return fmt.Errorf("clone failed: %w", err)
+	}
+	defer func() { _ = repo.Close() }()
+
+	tree, err := resolveTree(repo, sr.Path)
+	if err != nil {
+		return fmt.Errorf("resolving tree failed: %w", err)
+	}
+
+	if err := fetchObjects(ctx, repo.Storer, sr.URL, []plumbing.Hash{tree.Hash}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func fetchObjects(ctx context.Context, st storage.Storer, rawURL string, wants []plumbing.Hash) error {
+	url, err := transport.ParseURL(rawURL)
+	if err != nil {
+		return err //nolint: wrapcheck
+	}
+
+	cl := client.New()
+	session, err := cl.Handshake(ctx, &transport.Request{
+		URL:     url,
+		Command: transport.UploadPackService,
+	})
+	if err != nil {
+		return err //nolint: wrapcheck
+	}
+	defer func() { _ = session.Close() }()
+
+	return session.Fetch(ctx, st, &transport.FetchRequest{ //nolint: wrapcheck
+		Wants:    wants,
+		Progress: io.Discard,
+	})
+}
+
+func resolveTree(repo *git.Repository, path string) (*object.TreeEntry, error) {
+	head, err := repo.Head()
+	if err != nil {
+		return nil, err //nolint:wrapcheck
+	}
+
+	commit, err := repo.CommitObject(head.Hash())
+	if err != nil {
+		return nil, err //nolint:wrapcheck
+	}
+
+	tree, err := commit.Tree()
+	if err != nil {
+		return nil, err //nolint:wrapcheck
+	}
+
+	entry, err := tree.FindEntry(path)
+	if err != nil {
+		return nil, err //nolint:wrapcheck
+	}
+	return entry, nil
+}
+
+func sparseCloneOptions(url string, branch plumbing.ReferenceName) *git.CloneOptions {
+	return &git.CloneOptions{
+		URL:           url,
+		Depth:         1,
+		NoCheckout:    true,
+		SingleBranch:  true,
+		ReferenceName: branch,
+		Filter:        packp.FilterBlobNone(),
+		Progress:      io.Discard,
+	}
+}
